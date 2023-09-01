214d0b2dcb8370787da092320839c38b00442cb4
SergeyZhoo <sjoo@ya.ru>
2023-09-01 05:58:24 +0300

    v2.0

--- src/main/java/web/dao/CarDAOImpl.java
+++ src/main/java/web/dao/CarDAOImpl.java
@@ -1,16 +1,16 @@
-package web.service;
+package web.dao;
 
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import web.model.Car;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Component
-public class CarServiсeImpl implements CarService {
-    List<Car> carList = new ArrayList<>();
+@Repository
+public class CarDAOImpl implements CarDAO {
+    private final List<Car> carList = new ArrayList<>();
 
-    {
+    public CarDAOImpl() {
         carList.add(new Car(1L, "Corolla", 2022));
         carList.add(new Car(2L, "Civic", 2021));
         carList.add(new Car(3L, "Mustang", 2023));
@@ -18,11 +18,8 @@ public class CarServiсeImpl implements CarService {
         carList.add(new Car(5L, "Altima", 2022));
     }
 
+    @Override
     public List<Car> getCars(int numberOfCars) {
-        List<Car> selectedCars = new ArrayList<>();
-        for (int i = 0; i < numberOfCars && i < carList.size(); i++) {
-            selectedCars.add(carList.get(i));
-        }
-        return selectedCars;
+        return carList.subList(0, Math.min(numberOfCars, carList.size()));
     }
 }
